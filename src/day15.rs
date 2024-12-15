c341edfa37abd7223a9e015821be7ebbc38fd163
Dima Lazarev <dmitry.lazarev@gmail.com>
2024-12-15 13:13:14 +0200

    Update Day 15 Part 1

--- src/day15.rs
+++ src/day15.rs
@@ -1,31 +1,19 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 use std::collections::HashSet;
 
-#[derive(Copy, Clone, Eq, PartialEq)]
-enum Tile {
-    Wall,
-    Box,
-    Empty,
-    Robot,
-}
-
 struct WarehouseSetup {
-    warehouse_map: Vec<Vec<Tile>>,
+    walls: HashSet<(i32, i32)>,
+    boxes: HashSet<(i32, i32)>,
+    robot: (i32, i32),
     moves: Vec<(i32, i32)>,
 }
 
 #[aoc_generator(day15)]
 fn parse_input(input: &str) -> WarehouseSetup {
     use aoc_parse::{parser, prelude::*};
-    use Tile::*;
 
     let parser = parser!(
-        section(lines({
-            '#' => Wall,
-            'O' => Box,
-            '@' => Robot,
-            '.' => Empty
-        }+))
+        section(lines(any_char+))
         section(moves:lines({
                 '<' => (-1, 0),
                 '>' => (1, 0),
@@ -36,93 +24,71 @@ fn parse_input(input: &str) -> WarehouseSetup {
 
     let (warehouse_map, moves) = parser.parse(input).unwrap();
 
-    WarehouseSetup {
-        warehouse_map,
-        moves,
-    }
-}
-
-#[aoc(day15, part1)]
-fn part1(warehouse_setup: &WarehouseSetup) -> usize {
-    use Tile::*;
-    let mut warehouse_map = warehouse_setup.warehouse_map.clone();
-
-    let mut robot_position = (0, 0);
-
-    for (y, _) in warehouse_map.iter().enumerate() {
-        for (x, tile) in warehouse_map[y].iter().enumerate() {
-            if *tile == Robot {
-                robot_position = (x as i32, y as i32);
-                break;
-            }
-        }
-    }
-
-    let map_height = warehouse_map.len() as i32;
-    let map_width = warehouse_map[0].len() as i32;
-
-    for robot_move in warehouse_setup.moves.iter() {
-        let mut to_move = HashSet::new();
-
-        let (start, end, is_x) = match robot_move {
-            (0, -1) => (robot_position.1 - 1, 0, false),
-            (0, 1) => (robot_position.1 + 1, map_height, false),
-            (-1, 0) => (robot_position.0 - 1, 0, true),
-            (1, 0) => (robot_position.0 + 1, map_width, true),
-            _ => unreachable!(),
-        };
-
-        let range = if start < end {
-            (start..=end).collect::<Vec<_>>()
-        } else {
-            (end..=start).rev().collect::<Vec<_>>()
-        };
-
-        for x_or_y in range {
-            let (x, y) = if is_x {
-                (x_or_y, robot_position.1)
-            } else {
-                (robot_position.0, x_or_y)
-            };
-
-            if warehouse_map[y as usize][x as usize] == Box {
-                to_move.insert((x, y));
-            } else if warehouse_map[y as usize][x as usize] == Wall {
-                to_move.clear();
-                break;
-            } else {
-                let (new_x, new_y) = (
-                    robot_position.0 + robot_move.0,
-                    robot_position.1 + robot_move.1,
-                );
-
-                warehouse_map[robot_position.1 as usize][robot_position.0 as usize] = Empty;
-                warehouse_map[new_y as usize][new_x as usize] = Robot;
-
-                robot_position = (new_x, new_y);
-
-                break;
-            }
-        }
-
-        for (x, y) in to_move.iter() {
-            let (new_x, new_y) = (x + robot_move.0, y + robot_move.1);
-            warehouse_map[new_y as usize][new_x as usize] = Box;
-        }
-    }
+    let mut walls: HashSet<(i32, i32)> = HashSet::new();
+    let mut boxes: HashSet<(i32, i32)> = HashSet::new();
+    let mut robot: (i32, i32) = (0, 0);
 
     warehouse_map
         .iter()
         .enumerate()
         .flat_map(|(y, row)| row.iter().enumerate().map(move |(x, tile)| (x, y, tile)))
-        .filter_map(|(x, y, tile)| {
-            if *tile == Box {
-                Some(x + 100 * y)
-            } else {
-                None
+        .for_each(|(x, y, tile)| match tile {
+            '#' => {
+                walls.insert((x as i32, y as i32));
             }
-        })
-        .sum()
+            'O' => {
+                boxes.insert((x as i32, y as i32));
+            }
+            '@' => {
+                robot = (x as i32, y as i32);
+            }
+            _ => (),
+        });
+
+    WarehouseSetup {
+        walls,
+        boxes,
+        robot,
+        moves,
+    }
+}
+
+#[aoc(day15, part1)]
+fn part1(warehouse_setup: &WarehouseSetup) -> i32 {
+    let walls = &warehouse_setup.walls;
+    let mut boxes = warehouse_setup.boxes.clone();
+    let mut robot = warehouse_setup.robot;
+
+    for robot_move in warehouse_setup.moves.iter() {
+        let mut boxes_to_move = HashSet::new();
+
+        let mut position = (robot.0 + robot_move.0, robot.1 + robot_move.1);
+
+        loop {
+            if boxes.contains(&position) {
+                boxes_to_move.insert(position);
+                position.0 += robot_move.0;
+                position.1 += robot_move.1;
+            } else if walls.contains(&position) {
+                boxes_to_move.clear();
+                break;
+            } else {
+                robot.0 += robot_move.0;
+                robot.1 += robot_move.1;
+                break;
+            }
+        }
+
+        for moved_box in boxes_to_move.iter() {
+            boxes.remove(&moved_box);
+        }
+
+        for moved_box in boxes_to_move.iter() {
+            boxes.insert((moved_box.0 + robot_move.0, moved_box.1 + robot_move.1));
+        }
+    }
+
+    boxes.iter().map(|(x, y)| 100 * y + x).sum::<i32>()
 }
 
 #[aoc(day15, part2)]
