5db17b91f831ff08ba82cf3a271aa08888c2b3f6
Dima Lazarev <dmitry.lazarev@gmail.com>
2024-12-18 06:35:20 +0200

    Update Day 17

--- src/day17.rs
+++ src/day17.rs
@@ -1,126 +1,120 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[derive(Debug, Clone)]
-struct Computer {
-    register_a: usize,
-    register_b: usize,
-    register_c: usize,
-    program: Vec<usize>,
-    instruction_pointer: usize,
-    output: Vec<usize>,
+struct Memory {
+    register_a: u64,
+    register_b: u64,
+    register_c: u64,
 }
 
-impl Computer {
-    fn run_program(&mut self) {
-        while self.instruction_pointer < self.program.len() - 1 {
-            self.run_instruction();
-        }
+fn combo_operand(memory: &Memory, operand: u64) -> u64 {
+    match operand {
+        literal if literal <= 3 => literal,
+        4 => memory.register_a,
+        5 => memory.register_b,
+        6 => memory.register_c,
+        _ => unreachable!(),
     }
+}
 
-    fn print_output(&self) -> String {
-        let mut output = String::new();
+fn run_program(memory: &mut Memory, program: &Vec<u64>) -> Vec<u64> {
+    let mut instruction_pointer = 0;
+    let mut output = Vec::new();
 
-        if !self.output.is_empty() {
-            output.push_str(&self.output.first().unwrap().to_string());
-        }
-
-        self.output.iter().skip(1).for_each(|number| {
-            output.push(',');
-            output.push_str(&(*number).to_string());
-        });
-
-        output
-    }
-
-    fn run_instruction(&mut self) {
-        let opcode = self.program[self.instruction_pointer];
-        let operand = self.program[self.instruction_pointer + 1];
+    while instruction_pointer < program.len() - 1 {
+        let opcode = program[instruction_pointer];
+        let operand = program[instruction_pointer + 1];
 
         match opcode {
             0 => {
-                self.register_a >>= self.combo_operand(operand);
-                self.instruction_pointer += 2;
+                memory.register_a >>= combo_operand(&memory, operand);
+                instruction_pointer += 2;
             }
             1 => {
-                self.register_b ^= operand;
-                self.instruction_pointer += 2;
+                memory.register_b ^= operand;
+                instruction_pointer += 2;
             }
             2 => {
-                self.register_b = self.combo_operand(operand) & 7;
-                self.instruction_pointer += 2;
+                memory.register_b = combo_operand(&memory, operand) & 7;
+                instruction_pointer += 2;
             }
             3 => {
-                if self.register_a != 0 {
-                    self.instruction_pointer = operand;
+                if memory.register_a != 0 {
+                    instruction_pointer = operand as usize;
                 } else {
-                    self.instruction_pointer += 2;
+                    instruction_pointer += 2;
                 }
             }
             4 => {
-                self.register_b ^= self.register_c;
-                self.instruction_pointer += 2;
+                memory.register_b ^= memory.register_c;
+                instruction_pointer += 2;
             }
             5 => {
-                self.output.push(self.combo_operand(operand) & 7);
-                self.instruction_pointer += 2;
+                output.push(combo_operand(&memory, operand) & 7);
+                instruction_pointer += 2;
             }
             6 => {
-                self.register_b = self.register_a >> self.combo_operand(operand);
-                self.instruction_pointer += 2;
+                memory.register_b = memory.register_a >> combo_operand(&memory, operand);
+                instruction_pointer += 2;
             }
             7 => {
-                self.register_c = self.register_a >> self.combo_operand(operand);
-                self.instruction_pointer += 2;
+                memory.register_c = memory.register_a >> combo_operand(&memory, operand);
+                instruction_pointer += 2;
             }
             _ => unreachable!(),
         }
     }
 
-    fn combo_operand(&self, operand: usize) -> usize {
-        match operand {
-            literal if literal <= 3 => literal,
-            4 => self.register_a,
-            5 => self.register_b,
-            6 => self.register_c,
-            _ => unreachable!(),
-        }
+    output
+}
+
+fn print_output(output: &[u64]) -> String {
+    let mut printed_output = String::new();
+
+    if !output.is_empty() {
+        printed_output.push_str(&output.first().unwrap().to_string());
     }
+
+    output.iter().skip(1).for_each(|number| {
+        printed_output.push(',');
+        printed_output.push_str(&(*number).to_string());
+    });
+
+    printed_output
 }
 
 #[aoc_generator(day17)]
-fn parse_input(input: &str) -> Computer {
+fn parse_input(input: &str) -> (Memory, Vec<u64>) {
     use aoc_parse::{parser, prelude::*};
 
     let parser = parser!(
         registers:section(
-            line("Register A: " usize)
-            line("Register B: " usize)
-            line("Register C: " usize)
+            line("Register A: " u64)
+            line("Register B: " u64)
+            line("Register C: " u64)
         )
         program:section(
-            line("Program: " repeat_sep(usize, ","))
-        ) => Computer {
-            register_a: registers.0,
-            register_b: registers.1,
-            register_c: registers.2,
-            program,
-            instruction_pointer: 0,
-            output: vec![],
-        }
+            line("Program: " repeat_sep(u64, ","))
+        ) => (
+            Memory {
+                register_a: registers.0,
+                register_b: registers.1,
+                register_c: registers.2,
+            },
+            program
+        )
     );
 
     parser.parse(input).unwrap()
 }
 
 #[aoc(day17, part1)]
-fn part1(computer: &Computer) -> String {
-    let mut computer = computer.clone();
-    computer.run_program();
-    computer.print_output()
+fn part1((memory, program): &(Memory, Vec<u64>)) -> String {
+    print_output(&run_program(&mut memory.clone(), program))
 }
 
 #[aoc(day17, part2)]
-fn part2(computer: &Computer) -> usize {
+fn part2((memory, program): &(Memory, Vec<u64>)) -> u64 {
     unimplemented!()
 }
 
@@ -142,83 +136,69 @@ Program: 0,3,5,4,3,0";
 
     #[test]
     fn part1_program_example_1() {
-        let mut computer = Computer {
+        let mut memory = Memory {
             register_a: 0,
             register_b: 0,
             register_c: 9,
-            program: vec![2, 6],
-            instruction_pointer: 0,
-            output: vec![],
         };
 
-        computer.run_program();
+        run_program(&mut memory, &vec![2, 6]);
 
-        assert_eq!(computer.register_b, 1);
+        assert_eq!(memory.register_b, 1);
     }
 
     #[test]
     fn part1_program_example_2() {
-        let mut computer = Computer {
+        let mut memory = Memory {
             register_a: 10,
             register_b: 0,
             register_c: 0,
-            program: vec![5, 0, 5, 1, 5, 4],
-            instruction_pointer: 0,
-            output: vec![],
         };
 
-        computer.run_program();
-
-        assert_eq!(computer.print_output(), "0,1,2");
+        assert_eq!(
+            print_output(&run_program(&mut memory, &vec![5, 0, 5, 1, 5, 4])),
+            "0,1,2"
+        );
     }
 
     #[test]
     fn part1_program_example_3() {
-        let mut computer = Computer {
+        let mut memory = Memory {
             register_a: 2_024,
             register_b: 0,
             register_c: 0,
-            program: vec![0, 1, 5, 4, 3, 0],
-            instruction_pointer: 0,
-            output: vec![],
         };
 
-        computer.run_program();
+        let output = run_program(&mut memory, &vec![0, 1, 5, 4, 3, 0]);
 
-        assert_eq!(computer.print_output(), "4,2,5,6,7,7,7,7,3,1,0");
-        assert_eq!(computer.register_a, 0);
+        assert_eq!(print_output(&output), "4,2,5,6,7,7,7,7,3,1,0");
+        assert_eq!(memory.register_a, 0);
     }
 
     #[test]
     fn part1_program_example_4() {
-        let mut computer = Computer {
+        let mut memory = Memory {
             register_a: 0,
             register_b: 29,
             register_c: 0,
-            program: vec![1, 7],
-            instruction_pointer: 0,
-            output: vec![],
         };
 
-        computer.run_program();
+        run_program(&mut memory, &vec![1, 7]);
 
-        assert_eq!(computer.register_b, 26);
+        assert_eq!(memory.register_b, 26);
     }
 
     #[test]
     fn part1_program_example_5() {
-        let mut computer = Computer {
+        let mut memory = Memory {
             register_a: 0,
             register_b: 2_024,
             register_c: 43_690,
-            program: vec![4, 0],
-            instruction_pointer: 0,
-            output: vec![],
         };
 
-        computer.run_program();
+        run_program(&mut memory, &vec![4, 0]);
 
-        assert_eq!(computer.register_b, 44_354);
+        assert_eq!(memory.register_b, 44_354);
     }
 
     #[test]
