7a957117b2e0fc5848ab6fcc8c91df648049b491
Dima Lazarev <dmitry.lazarev@gmail.com>
2024-12-22 07:46:43 +0200

    Solve Day 21 Part 2

--- src/day21.rs
+++ src/day21.rs
@@ -1,8 +1,8 @@
 use crate::day21::Button::*;
 use crate::day21::Direction::*;
 use aoc_runner_derive::{aoc, aoc_generator};
-use lazy_static::lazy_static;
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, VecDeque};
+use std::hash::Hash;
 
 #[derive(Eq, PartialEq, Copy, Clone, Debug, Hash)]
 enum Direction {
@@ -20,29 +20,6 @@ enum Button {
 }
 
 type Position = (i8, i8);
-
-lazy_static! {
-    static ref NUMERIC_KEYPAD_LAYOUT: HashMap<Position, Button> = HashMap::from([
-        ((0, 0), Number(7)),
-        ((1, 0), Number(8)),
-        ((2, 0), Number(9)),
-        ((0, 1), Number(4)),
-        ((1, 1), Number(5)),
-        ((2, 1), Number(6)),
-        ((0, 2), Number(1)),
-        ((1, 2), Number(2)),
-        ((2, 2), Number(3)),
-        ((1, 3), Number(0)),
-        ((2, 3), Activate),
-    ]);
-    static ref DIRECTIONAL_KEYPAD_LAYOUT: HashMap<Position, Button> = HashMap::from([
-        ((1, 0), Move(Up)),
-        ((2, 0), Activate),
-        ((0, 1), Move(Left)),
-        ((1, 1), Move(Down)),
-        ((2, 1), Move(Right)),
-    ]);
-}
 
 #[aoc_generator(day21)]
 fn parse_input(input: &str) -> Vec<Vec<Button>> {
@@ -66,70 +43,165 @@ fn move_in_direction(position: &Position, direction: &Direction) -> Position {
     }
 }
 
-fn shortest_path(initial_state: &Vec<Position>, target: &Button) -> (Vec<Position>, usize) {
-    let mut visited: HashSet<Vec<Position>> = HashSet::from([initial_state.clone()]);
-    let mut queue = VecDeque::from([(initial_state.clone(), 0)]);
+fn distance(position_1: &Position, position_2: &Position) -> i8 {
+    (position_1.0 - position_2.0).abs() + (position_1.1 - position_2.1).abs()
+}
 
-    while let Some((state, buttons_pressed)) = queue.pop_front() {
-        for button in [Activate, Move(Up), Move(Down), Move(Left), Move(Right)] {
-            let mut level = 0;
-            let mut button = button;
+fn shortest_paths(
+    start_button: &Button,
+    end_button: &Button,
+    keypad: &[(Button, Position)],
+) -> Vec<Vec<Button>> {
+    let start_position = keypad
+        .iter()
+        .find(|(button, _)| button == start_button)
+        .unwrap()
+        .1;
+    let end_position = keypad
+        .iter()
+        .find(|(button, _)| button == end_button)
+        .unwrap()
+        .1;
 
-            loop {
-                match button {
-                    Move(direction) => {
-                        let mut new_state = state.clone();
-                        new_state[level] = move_in_direction(&state[level], &direction);
+    let mut distances: HashMap<Position, usize> = HashMap::from([(start_position, 0)]);
+    let mut queue: VecDeque<(Position, usize)> = VecDeque::from([(start_position, 0)]);
 
-                        if ((level == state.len() - 1
-                            && NUMERIC_KEYPAD_LAYOUT.contains_key(&new_state[level]))
-                            || (level < state.len() - 1
-                                && DIRECTIONAL_KEYPAD_LAYOUT.contains_key(&new_state[level])))
-                            && !visited.contains(&new_state)
-                        {
-                            visited.insert(new_state.clone());
-                            queue.push_back((new_state, buttons_pressed + 1));
-                        }
+    while let Some((position, distance)) = queue.pop_front() {
+        if position == end_position {
+            break;
+        }
 
-                        break;
-                    }
-                    Activate => {
-                        if level == state.len() - 1 {
-                            let new_output_button =
-                                NUMERIC_KEYPAD_LAYOUT.get(&state[level]).unwrap();
+        for direction in [Up, Down, Left, Right] {
+            let new_position = move_in_direction(&position, &direction);
 
-                            if target == new_output_button {
-                                return (state, buttons_pressed + 1);
-                            }
+            if keypad.iter().any(|(_, position)| *position == new_position)
+                && !distances.contains_key(&new_position)
+            {
+                distances.insert(new_position, distance + 1);
+                queue.push_back((new_position, distance + 1));
+            }
+        }
+    }
 
-                            break;
-                        } else {
-                            button = *DIRECTIONAL_KEYPAD_LAYOUT.get(&state[level]).unwrap();
+    let mut paths: Vec<Vec<Button>> = Vec::new();
+    let mut queue: Vec<(Position, Vec<Button>)> = vec![(start_position, vec![])];
 
-                            level += 1;
-                        }
-                    }
-                    _ => unreachable!(),
+    while let Some((position, path)) = queue.pop() {
+        if position == end_position {
+            let mut path = path.clone();
+            path.push(Activate);
+            paths.push(path);
+        }
+
+        let distance = *distances.get(&position).unwrap();
+
+        for direction in [Up, Down, Left, Right] {
+            let new_position = move_in_direction(&position, &direction);
+
+            if let Some(new_distance) = distances.get(&new_position) {
+                if *new_distance == distance + 1 {
+                    let mut new_path = path.clone();
+                    new_path.push(Move(direction));
+
+                    queue.push((new_position, new_path));
                 }
             }
         }
     }
 
-    unreachable!();
+    paths
+}
+
+fn init_costs(levels: usize) -> HashMap<(Button, Button), usize> {
+    let directional_keypad = vec![
+        (Move(Up), (1, 0)),
+        (Activate, (2, 0)),
+        (Move(Left), (0, 1)),
+        (Move(Down), (1, 1)),
+        (Move(Right), (2, 1)),
+    ];
+
+    let mut costs: HashMap<(Button, Button), usize> = HashMap::new();
+    let mut paths: HashMap<(Button, Button), Vec<Vec<Button>>> = HashMap::new();
+
+    for (start_button, start_position) in directional_keypad.iter() {
+        for (end_button, end_position) in directional_keypad.iter() {
+            costs.insert(
+                (*start_button, *end_button),
+                distance(start_position, end_position) as usize + 1,
+            );
+
+            paths.insert(
+                (*start_button, *end_button),
+                shortest_paths(start_button, end_button, &directional_keypad),
+            );
+        }
+    }
+
+    for _ in 1..levels {
+        let mut new_costs: HashMap<(Button, Button), usize> = HashMap::new();
+
+        for (start_button, _) in directional_keypad.iter() {
+            for (end_button, _) in directional_keypad.iter() {
+                let paths = paths.get(&(*start_button, *end_button)).unwrap();
+
+                let new_cost = paths
+                    .iter()
+                    .map(|path| {
+                        path.iter()
+                            .fold((0, Activate), |(cost, prev_button), button| {
+                                (cost + costs.get(&(prev_button, *button)).unwrap(), *button)
+                            })
+                    })
+                    .map(|(cost, _)| cost)
+                    .min()
+                    .unwrap();
+
+                new_costs.insert((*start_button, *end_button), new_cost);
+            }
+        }
+
+        costs = new_costs;
+    }
+
+    costs
 }
 
 fn shortest_sequence_len(code: &[Button], robots_count: usize) -> usize {
-    let mut robot_positions = vec![(2, 0); robots_count - 1];
-    robot_positions.push((2, 3));
-    let mut result = 0;
+    let numeric_keypad = vec![
+        (Number(7), (0, 0)),
+        (Number(8), (1, 0)),
+        (Number(9), (2, 0)),
+        (Number(4), (0, 1)),
+        (Number(5), (1, 1)),
+        (Number(6), (2, 1)),
+        (Number(1), (0, 2)),
+        (Number(2), (1, 2)),
+        (Number(3), (2, 2)),
+        (Number(0), (1, 3)),
+        (Activate, (2, 3)),
+    ];
 
-    for button in code.iter() {
-        let (new_positions, buttons_pressed) = shortest_path(&robot_positions, button);
-        robot_positions = new_positions;
-        result += buttons_pressed;
-    }
+    let costs = init_costs(robots_count - 1);
 
-    result
+    code.iter()
+        .fold((0, Activate), |(cost, prev_button), button| {
+            let cost = cost
+                + shortest_paths(&prev_button, button, &numeric_keypad)
+                    .iter()
+                    .map(|path| {
+                        path.iter()
+                            .fold((0, Activate), |(cost, prev_button), button| {
+                                (cost + costs.get(&(prev_button, *button)).unwrap(), *button)
+                            })
+                    })
+                    .map(|(cost, _)| cost)
+                    .min()
+                    .unwrap();
+
+            (cost, *button)
+        })
+        .0
 }
 
 fn numerical_value(code: &[Button]) -> usize {
